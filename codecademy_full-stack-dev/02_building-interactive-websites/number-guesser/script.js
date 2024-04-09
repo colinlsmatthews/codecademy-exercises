c68386cb6083d916ecdb6f3dc3e65e686da16ae0
colinlsmatthews <matthews.colin@proton.me>
2024-04-08 20:10:14 -0400

    number guesser project complete

--- codecademy_full-stack-dev/02_building-interactive-websites/number-guesser/script.js
+++ codecademy_full-stack-dev/02_building-interactive-websites/number-guesser/script.js
@@ -4,3 +4,28 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget() {
+    return Math.floor(Math.random() * 10);
+}
+
+function compareGuesses(userGuess, computerGuess, targetGuess) {
+    const userDiff = Math.abs(userGuess - targetGuess);
+    const computerDiff = Math.abs(computerGuess - targetGuess);
+    if (userDiff <= computerDiff) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function updateScore (winner) {
+    if (winner.toLowerCase() === "human") {
+        humanScore += 1;
+    } else if (winner.toLowerCase() === "computer") {
+        computerScore += 1;
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber += 1;
+}
