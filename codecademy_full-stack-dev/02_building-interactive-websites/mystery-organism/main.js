07a5e682960acf5caf91141d0623e0b0a1c81e81
Colin Matthews <matthews.colin@proton.me>
2024-05-19 10:11:08 -0400

    add debug config

--- codecademy_full-stack-dev/02_building-interactive-websites/mystery-organism/main.js
+++ codecademy_full-stack-dev/02_building-interactive-websites/mystery-organism/main.js
@@ -13,10 +13,39 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// Factory function
+let pAequor = (num, arr) => {
+  return {
+    specimenNum: num,
+    dna: arr,
+    mutate() {
+      const index = Math.floor(Math.random() * arr.length);
+      rBase = returnRandBase();
+      do {
+        arr[index] = rBase;
+      } while (arr[index] !== rBase);
+    },
+    compareDNA(pAequorCompare) {
+      let count = 0;
+      for (let i = 0; i < this.dna.length; i ++) {
+        if (this.dna[i] === pAequorCompare.dna[i]) {
+          count += 1;
+        }
+      }
+      const percentage = ((count / this.dna.length) * 100).toFixed(2);
+      console.log(`Specimen ${num} and ${pAequorCompare.specimenNum} have ${percentage}% DNA in Common.`)
+    }
+    
+  }
+}
 
+const testSample1 = pAequor(3, mockUpStrand());
+console.log(testSample1.dna);
+testSample1.mutate();
+console.log(testSample1.dna);
 
-
-
-
+const testSample2 = pAequor(4, mockUpStrand());
+console.log(testSample2.dna);
+testSample1.compareDNA(testSample2);
 
 
