7702d11889b81bcda733e0336cda4b5a3cc142e3
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2024-05-30 15:29:57 -0400

    message-selector

--- codecademy_full-stack-dev/02_building-interactive-websites/message-selector/main.js
+++ codecademy_full-stack-dev/02_building-interactive-websites/message-selector/main.js
@@ -1,1 +1,12 @@
-console.log("test");
+
+
+
+const fileUrl = 'codecademy_full-stack-dev\02_building-interactive-websites\message-selector\jokes.csv';
+fetch(fileUrl)
+    .then(response => response.text())
+    .then(data => {
+        const fileText = data;
+    })
+    .catch(error => console.error(''))
+
+console.log(data);
